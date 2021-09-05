91189ecddace3b050c93320bebfd858b55c24e5a
Eun Park <somnium.itineris@gmail.com>
2021-09-05 13:18:53 +0900

    Object Pool for Ammos and ammo system

---
+++ Assets/AmmoManager.cs
@@ -0,0 +1,94 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AmmoManager : MonoBehaviour
+{
+    private Transform sIGtransform;
+    private Transform aRtransform;
+
+    private int sig = 0;
+    private int maxSig = 0;
+    private int ar = 0;
+    private int maxAr = 0;
+
+    [SerializeField]
+    private int maxSIGAmmo = 5;
+    [Tooltip("19mm")]
+    [SerializeField]
+    private GameObject sIGBullet;
+    private GameObject[] sIGMagazine;
+
+    [SerializeField]
+    private int maxAR15Ammo = 10;
+    [Tooltip("5.56mm")]
+    [SerializeField]
+    private GameObject ar15Bullet;
+    private GameObject[] ar15Magazine;
+
+    private void Awake()
+    {
+        sIGtransform = FindObjectOfType<AmmoManager>().gameObject.transform.GetChild(0);
+        aRtransform = FindObjectOfType<AmmoManager>().gameObject.transform.GetChild(1);
+
+        sIGMagazine = new GameObject[maxSIGAmmo];
+        ar15Magazine = new GameObject[maxAR15Ammo];
+
+        maxSig = sIGMagazine.Length;
+        maxAr = ar15Magazine.Length;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        InstantiateAmmo();
+    }
+
+    private void InstantiateAmmo()
+    {
+        //SIG
+        for (int i = 0; i < maxSIGAmmo; i++)
+        {
+            sIGMagazine[i] = Instantiate(sIGBullet);
+            sIGMagazine[i].SetActive(false);
+            sIGMagazine[i].gameObject.transform.parent = sIGtransform;
+        }
+
+        //ar 15
+        for(int i = 0; i < maxAR15Ammo; i++)
+        {
+            ar15Magazine[i] = Instantiate(ar15Bullet);
+            ar15Magazine[i].SetActive(false);
+            ar15Magazine[i].gameObject.transform.parent = aRtransform;
+        }
+    }
+
+    public void FireBullet(Vector3 pos, Quaternion rot, BulletType bulletType)
+    {
+
+        if(bulletType == BulletType.SIG)
+        {
+            sIGMagazine[sig].transform.position = pos;
+            sIGMagazine[sig].transform.rotation = rot;
+            sIGMagazine[sig].gameObject.SetActive(true);
+            
+            sig++;
+
+            if (sig == maxSig)
+                sig = 0;
+        }
+
+        if(bulletType == BulletType.AR15)
+        {
+
+            ar15Magazine[ar].transform.position = pos;
+            ar15Magazine[ar].transform.rotation = rot;
+            ar15Magazine[ar].gameObject.SetActive(true);
+
+            ar++;
+
+            if (ar == maxAr)
+                ar = 0;
+        }
+    }
+
+}
---
+++ Assets/Scripts/Bullet.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+
+    [SerializeField]
+    private float bulletSpeed = 15f;
+
+    [SerializeField]
+    private float lifeTime = 3;
+
+    private Rigidbody rigid;
+    private Quaternion originRot;
+
+    private void Awake()
+    {
+        originRot = gameObject.transform.rotation;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigid = GetComponent<Rigidbody>();
+    }
+
+    private void OnEnable()
+    {
+        StartCoroutine(SetActiveFalseAmmo());
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        rigid.AddForce(transform.forward * bulletSpeed);
+    }
+
+    private IEnumerator SetActiveFalseAmmo()
+    {
+        yield return new WaitForSeconds(lifeTime);
+
+        gameObject.SetActive(false);
+        gameObject.transform.position = Vector3.zero;
+        gameObject.transform.rotation = originRot;
+        rigid.velocity = Vector3.zero;
+        rigid.angularVelocity = Vector3.zero;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Debug.Log("Bullet hit to " + other.gameObject.name);
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("Bullet hit to collision " + collision.gameObject.name);
+    }
+}
