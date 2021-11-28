dc829924c11a0b6e93976a22ac395f4f95551f29
Eun Park <somnium.itineris@gmail.com>
2021-11-29 01:06:53 +0900

    Trim Sheet and Base Scene

---
+++ Assets/Test/Test.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.Rendering;
+
+public class Test : MonoBehaviour
+{
+    [SerializeField]
+    private RenderPipelineAsset asset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        asset = GraphicsSettings.currentRenderPipeline;    
+    }
+}
